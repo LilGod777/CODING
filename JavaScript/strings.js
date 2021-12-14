686889eea4d236f018803e96fc832896d1bad43d
LilGod777 <jaredgrahammusic@gmail.com>
2021-12-14 11:09:53 -0500

    Updated strings.js file

--- JavaScript/strings.js
+++ JavaScript/strings.js
@@ -15,17 +15,3 @@ console.log(fullName)
 // fullName = fullName.split("")
 
 // console.log(fullName)
-
-let age = 25
-
-if (age < 21) {
-  console.log('You are too young little nigga')
-} else if (age > 21) {
-  console.log('GET LIT!')
-} else {
-  console.log('Ayyye you are 21, here is a free drink! Fuck a hoe! lol JK')
-}
-
-let dick = 'big'
-
-console.log(dick)
